a32ee3828793d754f7ccbb52afd2c80bb4a89f5a
Tiago Teruel Valle <tiagodw2k@hotmail.com>
2020-06-21 20:58:20 -0300

    first commit

---
+++ src/main/java/com/digitalcourse/springboot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.digitalcourse.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String helloMessage(){
+        return "Hello, meu primeiro Springboot app";
+    }
+}
